299ec9f573f6604e3c1be5fc701c978d88bd2fb3
Ethan <1028716801@qq.com>
2022-08-27 12:50:16 +0800

    测试接口

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="home">
     <h1>首页</h1>
+    <div>登录的用户名是{{ $store.state.username }}</div>
   </div>
 </template>
 <script>
+import { getUser } from '@/api/index'
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  async mounted () {
+    // 测试接口
+    const res = await getUser()
+    console.log(res)
+  }
 }
 </script>
